1f4fbea309bb87c9756848dbe9cb67d0c959ba88
bilold1n <babdumutalov436@gmail.com>
2024-07-04 16:14:54 +0500

    vazifa

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,10 @@ const Header = () => {
       const storedTheme = localStorage.getItem("theme");
       if (storedTheme) {
         setTheme(storedTheme);
-       window.document.documentElement.setAttribute("data-theme", storedTheme);
+        window?.document.documentElement.setAttribute(
+          "data-theme",
+          storedTheme
+        );
       }
     }, []);
 
@@ -17,7 +20,7 @@ const Header = () => {
       const newTheme = theme === "light" ? "dark" : "light";
       setTheme(newTheme);
       window.localStorage.setItem("theme", newTheme);
-      document.documentElement.setAttribute("data-theme", newTheme);
+      document?.documentElement.setAttribute("data-theme", newTheme);
     };
 
     return { theme, toggleTheme };
@@ -33,25 +36,27 @@ const Header = () => {
           </div>
           <div className="flex items-center justify-between w-full px-[20px]">
             <ul className="flex items-center gap-4">
-              <li>Shop ></li>
+              <li>Shop </li>
               <li>On Sale</li>
               <li>New Arrivals</li>
               <li>Brands</li>
             </ul>
             <div>
-            <label className="input input-bordered flex items-center gap-2 rounded-full w-[577px]">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-    className="h-4 w-4 opacity-70">
-    <path
-      fillRule="evenodd"
-      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-      clipRule="evenodd" />
-  </svg>
-  <input type="text" className="grow" placeholder="Search" />
-</label>
+              <label className="input input-bordered flex items-center gap-2 rounded-full w-[577px]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input type="text" className="grow" placeholder="Search" />
+              </label>
             </div>
           </div>
           <div>
