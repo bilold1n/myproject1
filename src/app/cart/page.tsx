61e6191581929870601722c6d32171c24d4a0186
bilold1n <babdumutalov436@gmail.com>
2024-07-16 21:07:10 +0500

    hoz

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import useGetData from "@/app/hooks/usegetdata";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Cart() {
   const { data } = useGetData("cart", false);
