dded77025d1f66e02ccb99e289796eba32a53df1
bilold1n <babdumutalov436@gmail.com>
2024-07-16 21:13:41 +0500

    hashadads

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Cart() {
   const { data } = useGetData("cart", false);
-  console.log("data", Object.keys(data[0]));
+  // console.log("data", Object.keys(data[0]));
 
   console.log(data);
   return <div>Cart</div>;
